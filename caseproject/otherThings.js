72abe4f5d617ed8fe632ae0a0ae7c39b89a96e61
KukuPRCC <iandewayne.merriam.32@student.prcc.edu>
2024-02-08 23:20:42 -0600

    Create otherThings.js

---
+++ caseproject/otherThings.js
@@ -0,0 +1,9 @@
+function calculateTotal()
+{
+    let quantity = document.getElementById("quantity").value.trim();
+}
+
+let pricePerDonut = 1.50; // Price for 1 donut 
+let totalCost = quanity * pricePerDonut
+
+document.getElementById("total").innerText = "total cost; $" 
