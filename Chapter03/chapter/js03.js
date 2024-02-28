789d1c765732be3fb63a9848a25aba067609b148
KukuPRCC <iandewayne.merriam.32@student.prcc.edu>
2024-02-27 22:24:02 -0600

    update js03.js

--- Chapter03/chapter/js03.js
+++ Chapter03/chapter/js03.js
@@ -12,76 +12,58 @@
 
 
      
-let Weekdays = [ "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",]
-function addWeekdays()
-{
-    let i = 0; 
-
-
-let headingCells = document.getElementsByTagName("th");
-
-while (i < 7)
- {
-    headingCells[1].innerHTML = weekDays(i);
-
-    i++
- }
-}
-
-
-window.addEventListener("load", addWeekdays);
-
-function showgames()
-{
-    for (let i = 0; i < gameDates.length; i++) 
-    {
-        let gameInfo = "";
-
-      switch (gameResults[i]) 
-      {
-        case "W": 
-        gameInfo += "<p class = 'win'> ";
-        break; 
-        case "L":
-         gameInfo += "<p class = 'lose'> ";
-        case "S":
-         gameInfo += "<p class = 'Suspended'> ";
-         case "P":
-         gameInfo += "<p class = 'Postponed'> ";
-      }
-
-        //Display game loco
-
-        if (gameLocations[i] === "h") {
-            gameInfo += "Vs. "
-        } else if (gameLocations[i] === "a") {
-            gameInfo += "@"; 
+     function addWeekdays() {
+        let Weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        let i = 0;
+        let headingCells = document.getElementsByTagName("th");
+        while (i < 7) {
+            headingCells[i + 1].innerHTML = Weekdays[i]; // Adjusted index by adding 1
+            i++;
         }
-
-        gameInfo += gameOppenents[i] + "<br>";
-
-        gameInfo += gameResults[i] + "; (" + runsScored[i] + "-" + runsAllowed[i] + ")"; 
-
-
-    if (gameInnings[i] < 5) 
-    {
-     gameInfo += " [" + gameInnings[i]+"]***";
     }
-    else if (gameInnings[i] < 9 )
-    {
-        gameInfo += " [" + gameInnings[i]+"]" + "J*";
     
+    window.addEventListener("load", addWeekdays);
+    
+    function showgames() {
+        for (let i = 0; i < gameDates.length; i++) {
+            let gameInfo = "";
+    
+            switch (gameResults[i]) {
+                case "W":
+                    gameInfo += "<p class='win'> ";
+                    break;
+                case "L":
+                    gameInfo += "<p class='lose'> ";
+                    break;
+                case "S":
+                    gameInfo += "<p class='Suspended'> ";
+                    break;
+                case "P":
+                    gameInfo += "<p class='Postponed'> ";
+                    break;
+            }
+    
+            // Display game location
+            if (gameLocations[i] === "h") {
+                gameInfo += "Vs. ";
+            } else if (gameLocations[i] === "a") {
+                gameInfo += "";
+            }
+    
+            gameInfo += gameOpponents[i] + "<br>";
+            gameInfo += gameResults[i] + "; (" + runsScored[i] + "-" + runsAllowed[i] + ")";
+    
+            if (gameInnings[i] < 5) {
+                gameInfo += " [" + gameInnings[i] + "]***";
+            } else if (gameInnings[i] < 9) {
+                gameInfo += " [" + gameInnings[i] + "]" + "J*";
+            } else if (gameInnings[i] > 9) {
+                gameInfo += " [" + gameInnings[i] + "]J"; 
+            }
+    
+            gameInfo += "</p>";
+    
+            let tableCell = document.getElementById(gameDates[i]);
+            tableCell.insertAdjacentHTML("beforeEnd", gameInfo);
+        }
     }
-    else if (gameInnings[i] > 9)
-{
-    gameInfo += "  [" + gameInnings[i] + "J"; 
-}
-
-    gameInfo += "</p>";
-
-    let tableCell = doctument.ElementsById(gameDates[i]);
-    tableCell.insertAdjacentHTML("beforeEnd",gameInfo)
-
-
-    }
-}
